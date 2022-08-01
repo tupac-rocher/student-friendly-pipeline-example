3283c10c8a127fed216d4e1f4f13da9fcadd55f0
tupac-rocher <tupac.rocher@etu.umontpellier.fr>
2022-08-01 11:36:39 +0200

    test new checkstyle configuration

---
+++ src/main/java/com/montpellier/Device.java
@@ -0,0 +1,26 @@
+package com.montpellier;
+public abstract class Device {
+    final static int third_Constant3 = 1000;
+    private int a;
+    private int b;
+    private int c;
+
+    public void foo(int c) {
+        // overlapping by method argument
+        c = c;            // violation, reference to instance variable "c" requires "this"
+    }
+    private void emptyLoop() {
+        for (int i = 0; i < 10; i++) { // violation
+        }
+
+        try { // violation
+
+        } catch (Exception e) {
+            // ignored
+        }
+        int test = 0;
+        if (true) test = 1; // violation
+        String text ="";
+        text = (text.equals("")) ? "" : text;
+    }
+}
