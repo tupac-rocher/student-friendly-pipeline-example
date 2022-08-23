e8adb8d12038c522b56b9f13e6c606759ccdb8f9
tupac-rocher <tupac.rocher@etu.umontpellier.fr>
2022-08-23 12:33:14 +0200

    add test on printHelloWorld

--- src/test/java/com/montpellier/ComputerTest.java
+++ src/test/java/com/montpellier/ComputerTest.java
@@ -1,10 +1,29 @@
 package com.montpellier;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 import static org.junit.Assert.*;
 
 public class ComputerTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @After
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
     @Test
     public void test_1(){
         Computer computer = new Computer();
@@ -16,5 +35,14 @@ public class ComputerTest {
         Computer computer = new Computer();
         computer.turnOnComputer();
         assertTrue(computer.isOn());
+    }
+
+    @Test
+    public void test_3(){
+        Computer computer = new Computer();
+        computer.turnOnComputer();
+        computer.openTerminal();
+        computer.printHelloWorld();
+        assertEquals("test\nHello World!\n", outContent.toString());
     }
 }
